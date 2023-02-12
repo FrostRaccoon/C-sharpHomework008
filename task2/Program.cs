30f0f0aaa565ebf8cd5c0d0ed79a08545adb1e9c
EnotWhyNot <danik_maltsev@mail.ru>
2023-02-12 13:36:51 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,62 @@
+﻿/* Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+var arr = Generate2DArray(2, 4);
+Print2DArray(arr);
+SmallestSumOfElements(arr);
+
+void SmallestSumOfElements(int[,] arr)
+{
+    int lineNum = 0;
+    int minSum = Int32.MaxValue;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sum = sum + arr[i, j];
+        }
+        if (sum < minSum)
+        {
+            minSum = sum;
+            lineNum++;
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine($"Номер строки с меньшей суммой элементов --> {lineNum}");
+}
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
